d6d6e131b45add0a8901b220060eeae7eb22d2d7
Anil <nlsinc.training@gmail.com>
2019-11-06 02:27:53 +0530

    Inheritance - super(), super vs this, toString(), overriding and method resolution

---
+++ Day7-Inheritance3/src/com/nlsinc/oops/TestInheritance.java
@@ -0,0 +1,17 @@
+package com.nlsinc.oops;
+
+public class TestInheritance {
+
+	public static void main(String[] args) {
+
+	         PrintDialog printer = new PrintDialog(4,1,20,100,90,200,100);
+	        /* System.out.println(printer.hashCode());
+	         System.out.println(Integer.toHexString(printer.hashCode()));
+		    System.out.println(printer);
+	         String str = printer.toString();
+	         System.out.println(str);*/
+		  
+	         printer.open();
+	}
+
+}
